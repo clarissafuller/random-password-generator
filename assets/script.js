0d88c2dac7129e6ea0c9056ce2057a9cc4b4e3d8
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-08-24 17:00:17 -0400

    adds psudo code to js file and adds user choice function

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
+//DOM ELEMENTS==============================================
+
+//DATA======================================================
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//FUNCTIONS==================================================
+function getUserChoice() {
+  var passwordLength = prompt(
+    "How long would you like your password to be?" +
+      "Please choose a length between 8 and 128 characters"
+  );
+}
+//prompt the user for how long they want the password
+//prompt the user if they want special characters or not
+//prompt the user if they want upper case or not
+//prompt the user if they want lower case or not
+//prompt the user if they want numbers or not
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +23,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+//USER INTERACTIONS===============================================
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//INITILAZATIONS====================================================
