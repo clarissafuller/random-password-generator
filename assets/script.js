be3a76a262655a25c045922030ac1901eabaead1
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-08-28 15:54:58 -0400

    adds psudo code to javascript file

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,27 @@
 var generateBtn = document.querySelector("#generate");
 
 //FUNCTIONS==================================================
-function getUserChoice() {
+function generatePassword() {
+  //prompt the user for how long they want the password
   var passwordLength = prompt(
     "How long would you like your password to be?" +
       "Please choose a length between 8 and 128 characters"
   );
+  if (passwordLength < 8 || passwordLength > 128) {
+    // do what needs to be done if the user made a mistake
+  }
+
+  //prompt the user if they want special characters or not
+  var passwordSpecialCharaters = confirm(
+    "Would you like to include special characters?"
+  );
+  //prompt the user if they want upper case or not
+  //prompt the user if they want lower case or not
+  //prompt the user if they want numbers or not
+
+  // last part of the function
+  return "A password to be generated later";
 }
-//prompt the user for how long they want the password
-//prompt the user if they want special characters or not
-//prompt the user if they want upper case or not
-//prompt the user if they want lower case or not
-//prompt the user if they want numbers or not
 
 // Write password to the #password input
 function writePassword() {
