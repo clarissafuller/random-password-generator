a2c6e6e1b6ad70833bb325d24606a2c313842c94
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-08-28 21:56:29 -0400

    adds length function and new write password function in javascript

--- assets/script.js
+++ assets/script.js
@@ -5,50 +5,49 @@
 var generateBtn = document.querySelector("#generate");
 
 //FUNCTIONS==================================================
-function generatePassword() {
+function writePassword() {
   //prompt the user for how long they want the password
-  var passwordLength = prompt(
-    "How long would you like your password to be?" +
-      "Please choose a length between 8 and 128 characters"
-  );
-  if (passwordLength < 8 || passwordLength > 128) {
-    // do what needs to be done if the user made a mistake
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+function getPasswordLength() {
+  var length =
+    parseInt[prompt("How long would you like your password to be? (8-128")];
+  if (length < 8 || length > 128) {
+    alert("Length must be 8-128 characters long.");
+    return getPasswordLength();
   }
-
-  //prompt the user if they want special characters or not
-  var passwordSpecialCharaters = confirm(
-    "Would you like to include special characters?"
-  );
-  //prompt the user if they want upper case or not
-  //prompt the user if they want lower case or not
-  //prompt the user if they want numbers or not
-
-  // last part of the function
-  return "A password to be generated later";
+  return length;
 }
 
 // Write password to the #password input
-function writePassword() {
-  var chosenCharacterSet = ' ';
-  var length = getPasswordLength
-var characterSets = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'abcdefghijklmnopqrstuvwxyz'.toUpperCase(),
-  special: '!@#$%^&*()_+'
-  numerical:'1234567890'
-};
+function generatePassword() {
+  var chosenCharacterSet = " ";
+  var length = getPasswordLength;
+  var characterSets = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "abcdefghijklmnopqrstuvwxyz".toUpperCase(),
+    special: "!@#$%^&*()_+",
+    numerical: "1234567890",
+  };
 
-for (var i = 0; i < Object.keys(characterSets).length; i++) {
-  if (confirm('Would you like to use '+ key + ' characters?')){
-  chosenCharacterSet += characterSets[key];
+  for (var i = 0; i < Object.keys(characterSets).length; i++) {
+    if (confirm("Would you like to use " + key + " characters?")) {
+      chosenCharacterSet += characterSets[key];
+    }
   }
-}
+  var password = " ";
 
-  passwordText.value = password;
+  for (var i = 0; i < length; i++)
+    password =
+      +chosenCharacterSet[
+        Math.floor(Math.random() * chosenCharacterSet.length)
+      ];
+  return password;
 }
 
 //USER INTERACTIONS===============================================
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//INITILAZATIONS====================================================
+generateBtn.addEventListener("click", generatePassword);
